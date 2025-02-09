45d18061893957adec3d7821a0741ee2adcbbb2c
axel husson <aaaxel.hsson@gmail.com>
2025-02-10 00:12:56 +0100

    Rendering a View for a Route

--- start/routes.ts
+++ start/routes.ts
@@ -11,5 +11,5 @@ import router from '@adonisjs/core/services/router'
 
 router.on('/').render('pages/home')
 router.get('/pokemons', async (ctx) => {
-  return 'My pokemons'
+  return ctx.view.render('pages/pokemons', { pokemon: 'Bulbizarre' })
 })
