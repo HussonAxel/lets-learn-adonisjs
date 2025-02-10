d88c89257443a9ce40347c93c78de1ab1150f621
axel husson <aaaxel.hsson@gmail.com>
2025-02-10 22:12:18 +0100

    Loading A Movie Using Route Parameters

--- start/routes.ts
+++ start/routes.ts
@@ -8,10 +8,15 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import fs from 'node:fs/promises'
+import app from '@adonisjs/core/services/app'
 
 router.on('/').render('pages/home').as('home')
+router.on('pokemons').render('pages/pokemons/index').as('pokemons.index')
 router
-  .get('/pokemons/my-pokemon', async (ctx) => {
-    return ctx.view.render('pages/pokemons', { pokemon: 'Bulbizarre' })
+  .get('/pokemons/:slug', async (ctx) => {
+    const url = app.makeURL(`resources/pokemons/${ctx.params.slug}.html`)
+    const pokemon = await fs.readFile(url, 'utf-8')
+    return ctx.view.render('pages/pokemons/show', { pokemon })
   })
   .as('pokemons.show')
