de4af37ffc4487a801d78147b270ccc00a26af44
axel husson <aaaxel.hsson@gmail.com>
2025-02-10 22:22:43 +0100

    add inbetween route

--- start/routes.ts
+++ start/routes.ts
@@ -12,10 +12,17 @@ import fs from 'node:fs/promises'
 import app from '@adonisjs/core/services/app'
 
 router.on('/').render('pages/home').as('home')
-router.on('pokemons').render('pages/pokemons/index').as('pokemons.index')
+router
+  .get('/pokemons', async (ctx) => {
+    const url = app.makeURL('resources/pokemons/index.html')
+    const pokemons = await fs.readFile(url, 'utf-8')
+    return ctx.view.render('pages/pokemons/index', { pokemons })
+  })
+  .as('pokemons.index')
+
 router
   .get('/pokemons/:slug', async (ctx) => {
-    const url = app.makeURL(`resources/pokemons/${ctx.params.slug}.html`)
+    const url = app.makeURL(`resources/pokemons/pokemon/${ctx.params.slug}.html`)
     const pokemon = await fs.readFile(url, 'utf-8')
     return ctx.view.render('pages/pokemons/show', { pokemon })
   })
